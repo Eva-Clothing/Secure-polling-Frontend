c89c2a9aa3160eaab81e22ba7205f81d242e0b4a
uttkarsh <uttkarshsingh789@gmail.com>
2021-01-31 21:33:00 +0530

    final commit

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,8 +7,10 @@ import Pic1 from "../../images/image-vote.jpg";
 import Pic2 from "../../images/image-data.jpg";
 import Pic3 from "../../images/image-bargraph.png";
 import Pic4 from "../../images/image-linegraph.jpg";
+import Pic5 from "../../images/hackMOL1.jpg"
+import Pic6 from "../../images/hackMOL2.jpg"
 import { Link } from "react-router-dom";
-import {signout,isAuthenticated} from "../../auth/helper"
+import { signout, isAuthenticated } from "../../auth/helper"
 
 
 
@@ -17,20 +19,20 @@ function Landing() {
     <>
       <nav className="navbar">
         <div className="nav-title">
-          <h1>Polling App</h1>
+          <h1>S-Polling App</h1>
         </div>
 
         <div className="navbar-links ml-3">
-        
-          <Link class ="link" to="/signin">
+
+          <Link class="link" to="/signin">
             Login
           </Link>
-          <Link class ="link" to="/signup">
+          <Link class="link" to="/signup">
             SignUp
           </Link>
-          {isAuthenticated()?<Link className="link" onClick={()=>{
-              signout()
-          }}>Logout</Link>:<div></div>  }
+          {isAuthenticated() ? <Link className="link" onClick={() => {
+            signout()
+          }}>Logout</Link> : <div></div>}
         </div>
       </nav>
       <div className="landing_page">
@@ -68,42 +70,42 @@ function Landing() {
         <div className="img-card">
           <img src={Pic2} alt="" />
           <div className="content">
-            <h2>Data </h2> 
+            <h2>Data </h2>
           </div>
         </div>
         <div className="img-card">
           <img src={Pic3} alt="" />
           <div className="content">
-            <h2>Bar Graph</h2> 
+            <h2>Bar Graph</h2>
           </div>
         </div>
         <div className="img-card">
           <img src={Pic4} alt="" />
           <div className="content">
-            <h2> Line Graph</h2> 
+            <h2> Line Graph</h2>
           </div>
         </div>{" "}
         <div className="img-card">
-          <img src={Pic1} alt="" />
+          <img src={Pic5} alt="" />
           <div className="content">
-            <h2>Rajesh Zuckerberg</h2> <p>Senior front-end dev</p>
+            <h2>Login and Register</h2> <p>Security Feature🔐</p>
           </div>
         </div>{" "}
         <div className="img-card">
-          <img src={Pic1} alt="" />
+          <img src={Pic6} alt="" />
           <div className="content">
-            <h2>Rajesh Zuckerberg</h2> <p>Senior front-end dev</p>
+            <h2>Face Detection</h2> <p>Security Feature🔐</p>
           </div>
         </div>
       </div>
       <footer className="mastfoot">
         <div >
-          <h3>Copyright of the Makers</h3>
-          </div>
-        <div className="footer_left">The Github repo is available at <GitHubIcon/></div>
-          
-            
-          
+          <h3>Made With 💙 by <strong>Runtime Terror's</strong></h3>
+        </div>
+        <div className="footer_left">The Github repo is available at <GitHubIcon /></div>
+
+
+
       </footer>
     </>
   );
